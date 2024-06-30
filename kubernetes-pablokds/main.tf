eed53e9a4fce6a2f37687d5ab6b481f8ec92e059
ioedumac <eacevedof@yahoo.es>
2024-06-30 17:36:45 +0200

    main.tf

--- kubernetes-pablokds/main.tf
+++ kubernetes-pablokds/main.tf
@@ -10,12 +10,13 @@ terraform {
 # Set the variable value in *.tfvars file
 # or using -var="do_token=..." CLI option
 variable "do_token" {
-    default = file(".env")
+    # default = file(".env") error no se puede usar funciones en do_token
+    default = ""
 }
 
-resource "digitalocean_ssh_key" "eduardoaf" {
-  name       = "eduardoaf"
-  public_key = file("id_rsa.pub")
+resource "digitalocean_ssh_key" "test_terraform" {
+  name       = "test_terraform"
+  public_key = file("~/.ssh/digocean-terraform.pub")
 }
 
 
@@ -26,11 +27,15 @@ provider "digitalocean" {
 
 # sacado de: 
 # https://github.com/pablokbs/peladonerd/blob/master/streaming/03-gitlab.tf/streaming/03-gitlab.tf
-resource "digitalocean_droplet" "gitlab" {
-  image     = "ubuntu-18-04-x64"
-  name      = "gitlab-1"
-  region    = "nyc1"
-  size      = "s-2vcpu-4gb"
-  user_data = "${file("userdata.yaml")}"
-  ssh_keys  = ["${digitalocean_ssh_key.pelado.fingerprint}"]
+resource "digitalocean_droplet" "dl_test_terraform" {
+  image     = "ubuntu-24-04-x64"
+  name      = "eaf-host-ubuntu-test-terraform"
+  region    = "fra1" #frankfurt
+  size      = "s-1vcpu-512mb-10gb"
+  # este fichero es un ejecutor de contenedores que se ejecuta cuando el droplet este levantado
+  # se llama tambien cloud-config
+  user_data = file("userdata.yaml")
+  monitoring = true
+  tags = ["eaf-host-ubuntu-test-terraform"]
+  ssh_keys  = ["${digitalocean_ssh_key.test_terraform.fingerprint}"]
 }
